3de6373b45020e8c553da976e5f05b42f96a4343
Bhaveshwagh04 <121219011+Bhaveshwagh04@users.noreply.github.com>
2024-09-18 15:44:22 +0530

    model evaluation added

--- networksecurity/components/model_evaluation.py
+++ networksecurity/components/model_evaluation.py
@@ -9,8 +9,8 @@ from networksecurity.utils.main_utils.utils import save_object,load_object,write
 from networksecurity.utils.ml_utils.model.estimator import ModelResolver
 from networksecurity.constant.training_pipeline import TARGET_COLUMN
 import pandas  as  pd
-import mlflow
-import mlflow.sklearn
+#import mlflow
+#import mlflow.sklearn
 class ModelEvaluation:
     def __init__(self,model_eval_config:ModelEvaluationConfig,
                     data_validation_artifact:DataValidationArtifact,
@@ -98,10 +98,11 @@ class ModelEvaluation:
             #dir_path=os.path.dirname(self.model_eval_config.model_evaluation_dir)
             #os.makedirs(dir_path,exist_ok=True)
         
-
+            #Save the report
             write_yaml_file(self.model_eval_config.report_file_path, model_eval_report)
             logging.info(f"Model evaluation artifact: {model_evaluation_artifact}")
             
+            """
             with mlflow.start_run():
                 f1_score=trained_metric.f1_score
                 precision_score=trained_metric.precision_score
@@ -112,7 +113,7 @@ class ModelEvaluation:
                 mlflow.log_metric("recall_score",recall_score)
                 
                 mlflow.sklearn.log_model(train_model,"model")
-                
+             """   
             return model_evaluation_artifact
         except Exception as e:
                 raise NetworkSecurityException(e,sys)
